bdbc8900875408fd0a5f6938bd486a2b308a965c
annaboyatyuk <annaboyatyuk@gmail.com>
2018-02-04 21:21:47 -0800

    add gh-pages branch

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
-
 var allLocations = [];
 
 function MakeLocation(name, minCustPerHour, maxCustPerHour, avgCookiesPerSale) {
@@ -157,4 +156,5 @@ function createNewLocation(event) {
   event.target.avgcookies.value = null;
 }
 storeForm.addEventListener('submit',(createNewLocation));
+
 
